d02faa8842807a5d5b1e4b86f537eea3b5c7b37d
tushar <iktushar2020@gmail.com>
2025-12-05 20:30:22 +0600

    user query write in server.ts file

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,35 @@
+import dotenv from 'dotenv'
 import express, { Request, Response } from 'express'
+import { Pool } from "pg"
+
+import path from 'path'
+
+dotenv.config({path: path.join(process.cwd(), '.env')})
 
 const app = express()
-const port = 5000
+const port = process.env.PORT || 5000
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL
+})
+
+const initDB = async () => {
+  await pool.query(`
+      CREATE TABLE IF NOT EXISTS users (
+      id SERIAL PRIMARY KEY,
+      name VARCHAR(255) NOT NULL,
+      email VARCHAR(255) NOT NULL UNIQUE,
+      password VARCHAR(255) NOT NULL CHECK (LENGTH(password) >= 6),
+      phone VARCHAR(20) NOT NULL,
+      role VARCHAR(10) NOT NULL CHECK (role IN ('admin', 'customer')),
+      CONSTRAINT email_lowercase CHECK (LOWER(email) = email),
+      created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+      updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    )
+  `)
+}
+
+initDB().catch(console.error)
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello World!!')
